976e05a128fa4dc4f6fcde775fecf4a668b29dd2
LowLuck <cfif905@bk.ru>
2021-04-24 16:34:44 +0300

    Bot fix

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,10 +2,9 @@ import discord
 from discord.ext import commands
 import requests
 
-TOKEN = "ODM0NDU3MDY2NzQ3NDYxNjMz.YIBKtA.tBJLp9I4zV79txwPWxyCQ2OEJxs"
+TOKEN = "ODM0NDU3MDY2NzQ3NDYxNjMz.YIBKtA.zmxaOKi87Bn4k1zjoRCeJUYKkpE"
 
 client = commands.Bot(command_prefix='mb!')
-
 
 @client.event
 async def on_ready():
@@ -17,22 +16,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    s = message.content.lower().split()
+    s = message.content.split()
 
     if s[0] == 'mb!':
-        if s[1] == 'getmemeuser':
-            q = f'https://flaskprojstud.herokuapp.com/export/{s[2]}/{s[3]}'
+        if s[1].lower() == 'getmemeuser':
+            q = f'http://127.0.0.1:5000/export/{s[2]}/{s[3]}'
             request = requests.get(q)
             if request:
-                answ = 'https://flaskprojstud.herokuapp.com/static/' + requests.get(q).json()
+                answ = 'http://127.0.0.1:5000/static/' + requests.get(q).json()
                 await message.channel.send(answ)
             else:
-                await message.chanel.send('Error in image getting')
+                await message.channel.send('Error in image getting')
+                await message.channel.send(s)
         elif s[1] == 'getmemepublic':
-            await message.channel.send('WIP')
+            await message.chanel.send('WIP')
         elif s[1] == 'help':
-            await message.channel.send('getmemeuser <username> <key> to get your meme')
-            await message.channel.send('getmemepublic <key> to get a public meme')
+            await message.chanel.send('getmemeuser <username> <key> to get your meme')
+            await message.chanel.send('getmemepublic <key> to get a public meme')
+
 
     # if 'mb!' in message.content.lower()[0:3]:
     #     if ' getmeme' in message.content.lower()[3:11]:
@@ -48,7 +49,5 @@ async def on_message(message):
     #     await message.channel.send(answ)
     # except Exception:
     #     pass
-
-
 
 client.run(TOKEN)
